fd4a79358a113c22c951c2c954498c5b3cc84c6f
sixthson6 <sudais2408@gmail.com>
2025-09-02 16:38:07 +0000

    Add everything

---
+++ src/test/java/com/quotegen/QuoteGenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.quotegen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuoteGenApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
