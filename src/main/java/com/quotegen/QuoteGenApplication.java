21f0210e576f1f6637e792d23a4ebdcf57fe8963
sixthson6 <sudais2408@gmail.com>
2025-09-02 10:25:25 +0000

    feat: Initialize App

---
+++ src/main/java/com/quotegen/QuoteGenApplication.java
@@ -0,0 +1,13 @@
+package com.quotegen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuoteGenApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QuoteGenApplication.class, args);
+    }
+
+}
